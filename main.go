66697ccde974dad03d2e89040f4ae5be78d31fec
idanieldrew <danielgia35@gmail.com>
2022-08-31 15:13:13 +0430

    Init

---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+func main() {
+	/*logTrashOld, _ := strconv.Atoi(os.Getenv("LOG_THRESHOLD"))
+	offset, _ := strconv.Atoi(os.Getenv("LOG_TIME_ZONE_OFFSET"))
+	timeZone, _ := timetime.FixedZone(os.Getenv("TIME_ZONE"), offset)*/
+}
