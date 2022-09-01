174d8e368deb9eaebd530eafd43875c2cd1fd2ae
idanieldrew <danielgia35@gmail.com>
2022-09-02 00:37:27 +0430

    Fix facade db

--- app/interfaces/db/mysql.go
+++
@@ -1,7 +0,0 @@
-package db
-
-import "database/sql"
-
-type Connection interface {
-	Connect() (*sql.DB, error)
-}
--- app/interfaces/logger/logger.go
+++
@@ -1,6 +0,0 @@
-package logger
-
-type Logger interface {
-	Info(msg string)
-	Error(msg string)
-}
