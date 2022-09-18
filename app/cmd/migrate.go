3553bb6ec5947d179474e1fae7155f3655eaa5f5
idanieldrew <danielgia35@gmail.com>
2022-09-18 23:14:49 +0430

    Add Migration:

--- app/cmd/migrate.go
+++ app/cmd/migrate.go
@@ -1,7 +1,28 @@
 package cmd
 
-import "github.com/urfave/cli/v2"
+import (
+	mysql2 "clean-golang/app/infrastructure/database/mysql"
+	"clean-golang/app/infrastructure/logger"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/mysql"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+	"github.com/urfave/cli/v2"
+)
 
 func Migrate(cCtx *cli.Context) error {
+	driver, err := mysql.WithInstance(mysql2.Db, &mysql.Config{})
+	if err != nil {
+		logger.Error("problem in migration")
+		return err
+	}
+
+	m, err := migrate.NewWithDatabaseInstance("file://migrations", "mysql", driver)
+	if err != nil {
+		logger.Error("problem in migration 2")
+		return err
+	}
+
+	_ = m.Steps(2)
 	return nil
 }
---
+++ migrations/1085649617_create_users_table.up.sql
@@ -0,0 +1,6 @@
+CREATE TABLE users
+(
+    id    integer unique,
+    name  varchar(40),
+    email varchar(40)
+);
